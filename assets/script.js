a108315e78b0eed5f0546b5dca7d47538e43b416
Neil <neil.roy63@gmail.com>
2021-06-14 17:34:18 +1000

    fix/updated script

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
 $(".textb").on("keyup", function (e) {
-  alert("OK");
+  // 13 equates to the enter key, checks for the value from .txtb class while also ensurin the val is not an empty string
+  if (e.keyCode == 13 && $(".textb").val() != "") {
+    var task = $("<div class='task'></div>").text($(".textb").val());
+  }
 });
