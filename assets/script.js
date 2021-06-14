8747322bbebd78f0175035fc3017b838a5ddae85
Neil <neil.roy63@gmail.com>
2021-06-14 17:40:26 +1000

    fix/updated script

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,12 @@ $(".textb").on("keyup", function (e) {
   // 13 equates to the enter key, checks for the value from .txtb class while also ensurin the val is not an empty string
   if (e.keyCode == 13 && $(".textb").val() != "") {
     var task = $("<div class='task'></div>").text($(".textb").val());
+    
+    var del = $("<i class='fas fa-trash-alt'></i>");
+
+    var check = $("<i class='fas fa-check'></i>");
+
+    task.append(del, check);
     $(".notcomp").append(task);
     // code to help clear the input
     $(".textb").val("");
